d06ac32a6def6e63db08f83715e921824cb969f5
Q00R <ripbaelol@gmail.com>
2023-11-20 19:24:38 +0200

    mock user model

---
+++ Backend/models/userModel.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zip: String,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false, // Default value if not provided
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,           // removes whitespace from both ends of a string
+  },
+});
+
+// Create the User model
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
