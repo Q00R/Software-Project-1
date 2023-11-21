bf03774594e2d8d5afaf2980fefe7f17de07c0d8
CrashOfTheTitans <omarmohamedasaad2004@gmail.com>
2023-11-21 21:44:08 +0200

    Darwizzy's Second Masterpiece

--- Backend/Models/Tickets.js
+++ Backend/Models/Tickets.js
@@ -3,59 +3,75 @@ const { ObjectId } = mongoose.Schema.Types;
 
 const TicketsSchema = new mongoose.Schema(
   {
-    _id: { type: ObjectId },
-    userId: {
+
+    _id: { type: ObjectId },  //Ticket ID
+    userId: {                 //User ID
         type: ObjectId,
         required: true,
         },
-        issueDate: {
+        issueDate: {          //Date of creation
         type: Date,
         required: true,
+        default: Date.now,
         },
-        expirationDate: {
+        expirationDate: {     //Date of expiration
         type: Date,
         required: false,
         },
-        title: {
+        title: {              //Title of the ticket
         type: String,
         required: true,
         },
-        description: {
+        description: {        //Description of the ticket
         type: String,
         required: true,
         },
-        status: { //open closed pending
+        status: {             //Opened, Closed, Pending
         type: String,
+        enum : ['Opened','Closed','Pending'],
         required: true,
         },
-        category: {
+        category: {         //A main category and a sub category
+          main: {
+            type: String,
+            enum: ['Software', 'Hardware', 'Network'],
+            required: true,
+          },
+          sub: {
+            type: String,
+            enum: [
+              'Desktops', 'Laptops', 'Printers', 'Servers', 'Networking equipment',
+              'Operating system', 'Application software', 'Custom software', 'Integration issues',
+              'Email issues', 'Internet connection problems', 'Website errors'
+            ],
+            required: false,
+          },
+        },
+        priority: {           //Low, Medium, High
         type: String,
+        enum : ['Low','Medium','High'],
         required: true,
         },
-        priority: {
-        type: String,
-        required: true,
+        assignedAgent: {       //The agent that will be assigned to the ticket (ask about this)
+            type: ObjectId,
+            required: false, 
         },
-        assignedAgent: {
-            type: ObjectId, // sanara
-            required: false, //ask about this
-        },
-        updates: {
-            type: {
+        updates: [            // user to agent -> {flag:0 with no description}, agent -> user {flag:1 with the update description}
+        {                     //updated to array as we can have multiple updates for 1 ticket
               flag: {
-                type: boolean,
+                type: Boolean,
                 required: true,
               },
               description: {
                 type: String,
                 required: false,
               },
+              timestamp: {
+                type: Date,
+                default: Date.now,
+              },
         },
-        required: false,
-    },
-
-    // CustomWorkflowToBeDone DataVis > CustomWorkflowToBeDone
-
+      ],
   }
 );
 
