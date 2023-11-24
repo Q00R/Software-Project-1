d589acb8669f1a80d127dde1b3242b66c42ad7ca
Q00R <ripbaelol@gmail.com>
2023-11-24 10:15:10 +0200

    changed workflows

--- Backend/models/workflowsModel.js
+++ Backend/models/workflowsModel.js
@@ -1,15 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
-const WorkflowsSchema = new mongoose.Schema(
-  {
+const WorkflowsSchema = new mongoose.Schema({
+  _id: { type: ObjectId }, //Workflow ID
+  issue: {
+    mainIssue: {
+      type: String,
+      enum: ["Software", "Hardware", "Network"],
+      required: true,
+    },
+    subIssue: {
+      type: String,
+      enum: [
+        "Desktops",
+        "Laptops",
+        "Printers",
+        "Servers",
+        "Networking equipment",
+        "Operating system",
+        "Application software",
+        "Custom software",
+        "Integration issues",
+        "Email issues",
+        "Internet connection problems",
+        "Website errors",
+      ],
+      required: true,
+    },
+  },
+  suggestions: [], //List of suggested solutions
+});
 
-        _id: { type: ObjectId },  //Workflow ID
-        ticketId: {                 //Ticket ID
-        type: ObjectId,
-        required: true,
-        },
-        suggestions: []     //List of suggested solutions
-    });
-
-module.exports = mongoose.model('Workflows', WorkflowsSchema);
+module.exports = mongoose.model("Workflows", WorkflowsSchema);
