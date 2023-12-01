cacab7673c57f20a468bb20fba34fb64d9cce0d2
janaelkasabyy <jana.elkasaby@student.giu-uni.de>
2023-12-01 20:01:29 +0200

    Agent view tickets and resolve them done

--- Backend/controllers/agentController.js
+++ Backend/controllers/agentController.js
@@ -8,86 +8,161 @@ const bcrypt = require("bcrypt");
 const nodemailer = require("nodemailer");
 
 const agentController = {
-    //respond to user ticket
-    respondToTicket: async (req, res) => {
-        try {
-            const id = req.params.id;
-            const agentId = req.body.agentId; //for now we will use the agentId from the body
-            const response = req.body.response;
-            const ticketStatus = req.body.ticketStatus;
 
-            const ticket = await ticketModel.findById(id);
-            if (!ticket) {
-                return res.status(400).json({ error: "Ticket not found!" });
-              }
-        
-              if (ticket.status === "Closed") {
-                return res.status(400).json({ error: "Ticket is closed!" });
-              }
-        
-              if (ticket.agentId !== agentId) {
-                return res.status(400).json({ error: "Wrong Agent!" });
-              }
-        
-              if (ticketStatus === "Closed") {
-                ticket.status = "Closed";
-                ticket.resolutionDate = Date.now();
-                await ticket.save();
-        
-                const agent = await supportAgentModel.findById(agentId);
-                if (agent) {
-                  agent.active_tickets.pull(id);
-                  agent.resolved_tickets.push(id);
-                  await agent.save();
-                }
-              }
-        
-              // Add the agent message to the ticket
-              ticket.Messages.AgentMessages.push({
-                message: response,
-              });
-              await ticket.save();
-        
-              // Send email to the user from the agent's email
-              const agent = await supportAgentModel.findById(agentId);
-              if (agent) {
-                const transporter = nodemailer.createTransport({
-                  service: "gmail",
-                  auth: {
-                    user: agent.user.email, // Use the agent's email as the sender
-                    pass: agent.user.hashedPassword, // I'm not sure about this. Securitists please verify
-                  },
-                });
-        
-                const user = await userModel.findById(ticket.userId);
-                if (user) {
-                  const mailOptions = {
-                    from: agent.user.email, // Use the agent's email as the sender
-                    to: user.email,
-                    subject: "Response to Your Ticket",
-                    text: `Dear ${user.username},\n\nYour support ticket has been updated. Here is the latest response:\n\n${response}`,
-                  };
-        
-                  transporter.sendMail(mailOptions, (error, info) => {
-                    if (error) {
-                      console.error(error);
-                    } else {
-                      console.log("Email sent: " + info.response);
-                    }
-                  });
-        
-                  return res.status(200).json({ message: "Response added!" });
-                } else {
-                  return res.status(400).json({ error: "User not found!" });
-                }
-              } else {
-                return res.status(400).json({ error: "Agent not found!" });
-              }
-            } catch (e) {
-              console.error(e);
-              return res.status(500).json({ error: "An Error has Occured" });
-            }
+  viewMyActiveTickets: async (req, res) => {
+    try {
+      const userID = req.params.id;
+      const myTicket = await ticketModel.findById(id);
+
+      if (myTicket.assignedAgent !== userID) {
+        return res.status(400).json({ error: "Error 400: Ticket was not assigned for you!" });
+      }
+      if (!myTicket) {
+        return res.status(400).json({ error: "Error 400: Unavailable ticket" });
+      }
+      if (myTicket.status === "Closed") {
+        return res.status(400).json({ error: "Error 400: Ticket is not active!" });
+      }
+      else {
+        return res.status(200).json({ message: "Active tickets", myTicket });
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error 500: Something is wrong, please try again!" });
+    }
+  },
+
+  viewMyResolvedTickets: async (req, res) => {
+    try {
+      const userID = req.params.id;
+      const myTicket = await ticketModel.findById(id);
+
+      if (myTicket.assignedAgent !== userID) {
+        return res.status(400).json({ error: "Error 400: Ticket was not assigned for you!" });
+      }
+      if (!myTicket) {
+        return res.status(400).json({ error: "Error 400: Unavailable ticket" });
+      }
+      if (myTicket.status === "Open") {
+        return res.status(400).json({ error: "Error 400: Ticket is still active!" });
+      }
+      else {
+        return res.status(200).json({ message: "Resolved tickets", myTicket });
+      }
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error 500: Something is wrong, please try again!" });
+    }
+  },
+
+  resolveTicket: async (req, res) => {
+    try {
+      const userID = req.params.id;
+      const myTicket = await ticketModel.findById(id);
+
+      if (myTicket.assignedAgent !== userID) {
+        return res.status(400).json({ error: "Error 400: Ticket was not assigned for you!" });
+      }
+      if (!myTicket) {
+        return res.status(400).json({ error: "Error 400: Unavailable ticket" });
+      }
+      if (myTicket.status === "Closed") {
+        return res.status(400).json({ error: "Error 400: Ticket has been already resolved!" });
+      }
+      else {
+        myTicket.status = "Closed";
+        myTicket.resolutionDate = Date.now();
+        myTicket.answer.title = req.body.title;
+        myTicket.answer.description = req.body.description;
+        await myTicket.save();
+        return res.status(200).json({ message: "Ticket is updated into resolved!" });
+      }
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Error 500: Something is wrong, please try again!" });
+    }
+  },
+  //respond to user ticket
+  respondToTicket: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const agentId = req.body.agentId; //for now we will use the agentId from the body
+      const response = req.body.response;
+      const ticketStatus = req.body.ticketStatus;
+
+      const ticket = await ticketModel.findById(id);
+      if (!ticket) {
+        return res.status(400).json({ error: "Ticket not found!" });
+      }
+
+      if (ticket.status === "Closed") {
+        return res.status(400).json({ error: "Ticket is closed!" });
+      }
+
+      if (ticket.agentId !== agentId) {
+        return res.status(400).json({ error: "Wrong Agent!" });
+      }
+
+      if (ticketStatus === "Closed") {
+        ticket.status = "Closed";
+        ticket.resolutionDate = Date.now();
+        await ticket.save();
+
+        const agent = await supportAgentModel.findById(agentId);
+        if (agent) {
+          agent.active_tickets.pull(id);
+          agent.resolved_tickets.push(id);
+          await agent.save();
+        }
+      }
+
+      // Add the agent message to the ticket
+      ticket.Messages.AgentMessages.push({
+        message: response,
+      });
+      await ticket.save();
+
+      // Send email to the user from the agent's email
+      const agent = await supportAgentModel.findById(agentId);
+      if (agent) {
+        const transporter = nodemailer.createTransport({
+          service: "gmail",
+          auth: {
+            user: agent.user.email, // Use the agent's email as the sender
+            pass: agent.user.hashedPassword, // I'm not sure about this. Securitists please verify
           },
-        };
+        });
+
+        const user = await userModel.findById(ticket.userId);
+        if (user) {
+          const mailOptions = {
+            from: agent.user.email, // Use the agent's email as the sender
+            to: user.email,
+            subject: "Response to Your Ticket",
+            text: `Dear ${user.username},\n\nYour support ticket has been updated. Here is the latest response:\n\n${response}`,
+          };
+
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              console.error(error);
+            } else {
+              console.log("Email sent: " + info.response);
+            }
+          });
+
+          return res.status(200).json({ message: "Response added!" });
+        } else {
+          return res.status(400).json({ error: "User not found!" });
+        }
+      } else {
+        return res.status(400).json({ error: "Agent not found!" });
+      }
+    } catch (e) {
+      console.error(e);
+      return res.status(500).json({ error: "An Error has Occured" });
+    }
+  },
+};
 
 module.exports = agentController;
