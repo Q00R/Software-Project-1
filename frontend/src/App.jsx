71143fe2142082ed116755b9436b57cb935f93f8
Q00R <ripbaelol@gmail.com>
2023-12-21 01:10:43 +0200

    some more edits

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 // import '../public/styles/bootstrap.min.css'
 import { Route, Routes } from "react-router-dom";
+import "./index.css";
 import Homepage from "./pages/HomePage";
-import Login from "./pages/login";
-import Register from "./pages/register";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import MFA from "./pages/MFA";
+
 function App() {
   return (
     <>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 import withMT from "@material-tailwind/html/utils/withMT";
 export default withMT({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {},
       fontFamily: {},
       screens: {},
     },
+  
   },
   plugins: [require("daisyui")],
-});
+}); 
