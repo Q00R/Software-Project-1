cfc2086545b7a05ca5749e6beeb38f4d95b6042c
Q00R <ripbaelol@gmail.com>
2023-12-21 01:27:35 +0200

    aha

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,12 @@
-import withMT from "@material-tailwind/html/utils/withMT";
-export default withMT({
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {},
-      fontFamily: {},
-      screens: {},
-    },
-  
+    extend: {},
   },
   plugins: [require("daisyui")],
-}); 
+}
+
