565d4c10339a8177d67f84a5c10fd10ce5f7282a
Deebo86 <21meldeeb.86@gmail.com>
2023-12-21 17:20:17 +0200

    some updates

--- Backend/controllers/clientController.js
+++ Backend/controllers/clientController.js
@@ -1,7 +1,98 @@
 const ticketModel = require("../Models/ticketModel");
 const workflowModel = require("../models/workflowsModel");
 const supportAgentModel = require("../Models/supportAgentModel");
-const {Queue, HighPriority, MediumPriority, LowPriority} = require("../Queue");
+const { Queue, HighPriority, MediumPriority, LowPriority } = require("../Queue");
+
+//periodically assign tickets to agents
+function isFree(agent) {
+    return agent.active_tickets.Software.length + agent.active_tickets.Hardware.length + agent.active_tickets.Network.length < 5;
+}
+
+function assignAgent(queue, noAgent, agents, issue, ticket) {
+    const agent = agents.find({ main_role: issue });
+    if (isFree(otherAgent)) {
+        ticket.assignedAgent = otherAgent._id;
+        ticket.ticketStatus = "In Progress";
+        agent.active_tickets[ticket.mainIssue].push(ticket._id);
+    }
+    else {
+        noAgent.push(ticket);
+        queue.enqueue(ticket);
+    }
+}
+setInterval(async () => {
+    agents = await supportAgentModel.find({ status: "Activated" });
+    //loop through all agents
+    const noAgentHigh = [];
+    const noAgentMid = [];
+    const noAgentLow = [];
+    while (!HighPriority.isEmpty()) {
+        const ticket = HighPriority.dequeue();
+        assignAgent(HighPriority, noAgentHigh, agents, ticket.mainIssue, ticket)
+        if (HighPriority.front() == noAgentHigh[0])
+            break;
+    }
+    while (!MediumPriority.isEmpty()) {
+        const ticket = MediumPriority.dequeue();
+        const agent = agents.find({ main_role: ticket.mainIssue });
+        if (isFree(agent)) {
+            ticket.assignedAgent = agent._id;
+            ticket.ticketStatus = "In Progress";
+            agent.active_tickets[ticket.mainIssue].push(ticket._id);
+        }
+        else {
+            switch (ticket.mainIssue) {
+                case "Software":
+                    {
+                        assignAgent(MediumPriority, noAgentMid, agents, "Hardware", ticket)
+                        break;
+                    }
+                case "Hardware":
+                    {
+                        assignAgent(MediumPriority, noAgentMid, agents, "Network", ticket)
+                        break;
+                    }
+                case "Network":
+                    {
+                        assignAgent(MediumPriority, noAgentMid, agents, "Software", ticket)
+                        break;
+                    }
+            }
+        }
+        if (MediumPriority.front() == noAgentMid[0])
+            break;
+    }
+    while (!LowPriority.isEmpty()) {
+        const ticket = LowPriority.dequeue();
+        const agent = agents.find({ main_role: ticket.mainIssue });
+        if (isFree(agent)) {
+            ticket.assignedAgent = agent._id;
+            ticket.ticketStatus = "In Progress";
+            agent.active_tickets[ticket.mainIssue].push(ticket._id);
+        }
+        else {
+            switch (ticket.mainIssue) {
+                case "Software":
+                    {
+                        assignAgent(LowPriority, noAgentLow, agents, "Network", ticket)
+                        break;
+                    }
+                case "Hardware":
+                    {
+                        assignAgent(LowPriority, noAgentLow, agents, "Software", ticket)
+                        break;
+                    }
+                case "Network":
+                    {
+                        assignAgent(LowPriority, noAgentLow, agents, "Hardware", ticket)
+                        break;
+                    }
+            }
+        }
+        if (LowPriority.front() == noAgentLow[0])
+            break;
+    }
+}, 1000 * 60 * 5);
 
 const clientController = {
     generateWorkflow: async (req, res) => {
