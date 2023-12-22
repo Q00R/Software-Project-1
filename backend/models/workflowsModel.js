2be269e6b0a27f9e4854a3f5123366d24fab1bbc
Mariam Eldeeb <21meldeeb.86@gmail.com>
2023-12-23 01:13:09 +0200

    modified workflow model again

--- backend/models/workflowsModel.js
+++ backend/models/workflowsModel.js
@@ -2,56 +2,54 @@ const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Types;
 
 const WorkflowsSchema = new mongoose.Schema({
-  issues: {
-    Software: [
-      {
-        subIssue: {
-          type: String,
-          enum: [
-            "Operating system",
-            "Application software",
-            "Custom software",
-            "Integration issues",
-          ],
-          required: true,
-        },
-        commonIssues: [], //List of common issues
-        suggestions: [], //List of suggested solutions
+  Software: [
+    {
+      subIssue: {
+        type: String,
+        enum: [
+          "Operating system",
+          "Application software",
+          "Custom software",
+          "Integration issues",
+        ],
+        required: true,
       },
-    ],
-    Hardware: [
-      {
-        subIssue: {
-          type: String,
-          enum: [
-            "Desktops",
-            "Laptops",
-            "Printers",
-            "Servers",
-            "Netowrking equipment",
-          ],
-          required: true,
-        },
-        commonIssues: [], //List of common issues
-        suggestions: [], //List of suggested solutions
+      commonIssues: [], //List of common issues
+      suggestions: [], //List of suggested solutions
+    },
+  ],
+  Hardware: [
+    {
+      subIssue: {
+        type: String,
+        enum: [
+          "Desktops",
+          "Laptops",
+          "Printers",
+          "Servers",
+          "Netowrking equipment",
+        ],
+        required: true,
       },
-    ],
-    Netwrok: [
-      {
-        subIssue: {
-          type: String,
-          enum: [
-            "Email issues",
-            "Internet Connection Problems",
-            "Website Errors",
-          ],
-          required: true,
-        },
-        commonIssues: [], //List of common issues
-        suggestions: [], //List of suggested solutions
+      commonIssues: [], //List of common issues
+      suggestions: [], //List of suggested solutions
+    },
+  ],
+  Netwrok: [
+    {
+      subIssue: {
+        type: String,
+        enum: [
+          "Email issues",
+          "Internet Connection Problems",
+          "Website Errors",
+        ],
+        required: true,
       },
-    ],
-  },
+      commonIssues: [], //List of common issues
+      suggestions: [], //List of suggested solutions
+    },
+  ],
 });
 
 module.exports = mongoose.model("Workflows", WorkflowsSchema);
