2d8337bf93b55a6eb626eb91d9c239ae073a1584
Q00R <ripbaelol@gmail.com>
2023-12-24 00:53:24 +0200

    added fancy cards + half baked functionality

--- frontend/src/pages/adminDashboard.jsx
+++ frontend/src/pages/adminDashboard.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import axios from "axios";
 // import the app.css file
 
 const AdminDashboard = () => {
@@ -7,11 +7,14 @@ const AdminDashboard = () => {
 
   const fetchUsers = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/admin/viewAllUsers', { withCredentials: true });
+      const response = await axios.get(
+        "http://localhost:3000/admin/viewAllUsers",
+        { withCredentials: true }
+      );
       const { data } = response;
       setUsers(data || []);
     } catch (error) {
-      console.error('Error fetching users:', error);
+      console.error("Error fetching users:", error);
     }
   };
 
@@ -20,47 +23,98 @@ const AdminDashboard = () => {
     fetchUsers();
   }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
+  const changeRole = async (userId, newRole) => {
+    try {
+      const response = await axios({
+        method: "put",
+        url: "http://localhost:3000/admin/changeRole",
+        headers: { withCredentials: true },
+        data: {
+          userId,
+          newRole, // This is the body part
+        },
+      });
+      const { data } = response;
+      if(data.success){
+        fetchUsers();
+        alert("Role changed successfully");
+      }else{
+        console.error('Failed to change role:', data.message);
+      }
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
   return (
-    <div>
-    {users.map((user) => (
-      <div key={user._id} className="mb-4 ml-4 w-3/4">
-        <div className="card w-full bg-primary text-primary-content">
-          <div className="card-body">
-            <h2 className="card-title text-lg">User</h2>
-            {/* Use text-lg or other text size classes as needed */}
-            <p className="text-xl">User ID: {user._id}</p>
-            {/* Adjust the text size based on your preference, e.g., text-xl, text-lg, etc. */}
-            <p className="text-xl">User Name: {user.username} </p>
-            <p className="text-xl">Email: {user.email} </p>
-            <p className="text-xl">DOB: {user.DOB} </p>
-            <p className="text-xl">Role: {user.role}</p>
-            <p className="text-xl">Status: {user.status} </p>
-          
-          <div className="card-actions justify-end">
-            <button className="btn">Buy Now</button>
-          </div>
+    <div className="flex flex-col items-center">
+      {users.map((user) => (
+        <div
+          key={user._id}
+          className="m-4 w-1/2 transform transition duration-500 ease-in-out hover:scale-105"
+        >
+          <div className="card w-full bg-gradient-to-r from-blue-400 via-green-500 to-red-500 text-white shadow-lg rounded-lg p-6">
+            <div className="card-body">
+              <h2 className="card-title text-lg">User</h2>
+              <p className="text-base">User ID: {user._id}</p>
+              <p className="text-base">User Name: {user.username} </p>
+              <p className="text-base">Email: {user.email} </p>
+              <p className="text-base">DOB: {user.DOB} </p>
+              <p className="text-base">Role: {user.role}</p>
+              <p className="text-base">Status: {user.status} </p>
+              <div className="card-actions justify-end">
+                <div className="dropdown">
+                  <button
+                    tabIndex={0}
+                    role="button"
+                    className="btn bg-white rounded-full"
+                  >
+                    Change Role
+                  </button>
+                  <ul
+                    tabIndex={0}
+                    className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 text-black"
+                  >
+                    <li>
+                      <a onClick={() => changeRole(user._id, "admin")}>admin</a>
+                    </li>
+                    <li>
+                      <a onClick={() => changeRole(user._id, "agent")}>agent</a>
+                    </li>
+                    <li>
+                      <a onClick={() => changeRole(user._id, "manager")}>
+                        manager
+                      </a>
+                    </li>
+                    <li>
+                      <a onClick={() => changeRole(user._id, "client")}>
+                        client
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    ))}
-  </div>
-  
-    // <div>
-    //   <h1>Admin Dashboard</h1>
-    //   <ul className="user-list">
-    //     {users.map((user) => (
-    //       <li key={user._id}>
-    //         <p>User Name: {user.username} </p>
-    //         <p>Email: {user.email} </p>
-    //         <p>DOB: {user.DOB} </p>
-    //         <p>Role: {user.role}</p>
-    //         <p>Status: {user.status} </p>
-    //         <br></br>
-    //       </li>
-    //     ))}
-    //   </ul>
-    // </div>
-  );  
+      ))}
+    </div>
+  );
 };
+
+// <div>
+//   <h1>Admin Dashboard</h1>
+//   <ul className="user-list">
+//     {users.map((user) => (
+//       <li key={user._id}>
+//         <p>User Name: {user.username} </p>
+//         <p>Email: {user.email} </p>
+//         <p>DOB: {user.DOB} </p>
+//         <p>Role: {user.role}</p>
+//         <p>Status: {user.status} </p>
+//         <br></br>
+//       </li>
+//     ))}
+//   </ul>
+// </div>
 
 export default AdminDashboard;
