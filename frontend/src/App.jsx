362ba1036df6337713004b7a329885ba6f813d91
Q00R <ripbaelol@gmail.com>
2023-12-23 20:54:30 +0200

    added themechanger and fixed navbar 1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,23 @@
 // import '../public/styles/bootstrap.min.css'
 import { useEffect, useState  } from "react";
 import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
-import {
-  HomePage,
-  Login,
-  Register,
-  MFA,
-  Admin,
-  Knowledgebase,
-  AgentDashboard,
-  ClientHome,
-  TrySearchParams,
-  viewProfile,
-} from "./pages";
-import "./index.css"; // Import your CSS file
+import "./index.css";
+import Homepage from "./pages/HomePage";
+import Login from "./pages/login";
+import Register from "./pages/Register";
+import MFA from "./pages/MFA";
+import Admin from "./pages/adminDashboard";
+import Knowledgebase from "./pages/Knowledgebase";
 import Navbar from "./components/Navbar";
+import ViewProfile from "./pages/viewProfile";
+import ClientHome from "./pages/ClientHome";
+import AgentDashboard from "./pages/AgentDashboard";
 import Footer from "./components/Footer";
 function App() {
-
-  const [update, setUpdate] = useState(false);
   
-
-  console.log(location.pathname); // result: '/secondpage'
-
-  useEffect(() => {
-    console.log(location.pathname); // result: '/secondpage'
-    setUpdate(!update);
-  }, [location]);
-
-  return (
+  return  (
     <>
       <Navbar />
-      <div
-        style={{
-          position: "fixed",
-          width: "100%",
-          height: "93%",
-          top: "7%",
-          left: 0,
-          overflow: "scroll",
-        }}>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
@@ -49,7 +27,6 @@ function App() {
         <Route path="/viewProfile" element={<ViewProfile />} />
       </Routes>
       <Footer />
-      </div>
     </>
   );
 }
