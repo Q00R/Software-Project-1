d2fe07230aa353cb052e5f33240ae7ce29760b64
Mariam Eldeeb <21meldeeb.86@gmail.com>
2023-12-25 20:19:00 +0200

    added the photos

---
+++ frontend/src/assets/profilePhotos/index.js
@@ -0,0 +1,19 @@
+import ali from './ali.jpg';
+import mariamAmr from './mariam.jpg';
+import mariam from './mariam.jpeg';
+import mostafa from './mostafa.jpg';
+import omar from './omar.jpg';
+import jana from './jana.jpg';
+import chilli from './chilli.jpg';
+import mina from './mina.jpg';
+
+export {
+    ali,
+    mariamAmr,
+    mariam,
+    mostafa,
+    omar,
+    jana,
+    chilli,
+    mina,
+}
--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import AboutUsCard from './AboutUsCard'
+import {ali, mina, mostafa, jana, mariam, mariamA, chilli, omar} from '..assets/profilePhotos'
 
 const AboutUs = () => {
   return (
     <div className='grid grid-rows-2 grid-flow-col auto-rows-auto bg-white m-3 rounded-box shadow-md overflow-auto scrollbar-thin scrollbar-track-slate-50 scrollbar-thumb-slate-100'>
       <AboutUsCard 
-    //   imgURL={}
-    //   name={}
-    //   profileURL={}/
-    />
+      imgURL={ali}
+      name="Ali Zein"
+      profileURL={mina}/>
       <AboutUsCard />
       <AboutUsCard />
       <AboutUsCard />
