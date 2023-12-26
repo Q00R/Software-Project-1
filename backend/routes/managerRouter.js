709f41af7853bdf327774a4e4aa882dbbe3904cf
Mariam Reda Rizkallah <mariamrizkallah1@gmail.com>
2023-12-26 20:10:44 +0200

    renamed files and added missing authentocation

--- backend/routes/managerRouter.js
+++ backend/routes/managerRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const managerController = require("../controllers/managerController");
 const authMiddleware=require('../middleware/authorizationMiddleware');
 
-//router.get("/generateAnalytics/:type",authMiddleware(['manager']), managerController.generateAnalytics);
-router.get("/generateAnalytics/:type", managerController.generateAnalytics);
+router.get("/generateAnalytics/:type",authMiddleware(['manager']), managerController.generateAnalytics);
+//router.get("/generateAnalytics/:type", managerController.generateAnalytics);
 
 module.exports = router;
